43cea24db32e1879494e6ef6b86afa574daa5df2
Daae-Kim <wlsemrdl1212@naver.com>
2025-07-27 16:35:05 +0900

    Initial Commit

---
+++ src/main/java/com/example/dnd_13th_9_be/Dnd13th9BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dnd_13th_9_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dnd13th9BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dnd13th9BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dnd_13th_9_be/Dnd13th9BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dnd_13th_9_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dnd13th9BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
