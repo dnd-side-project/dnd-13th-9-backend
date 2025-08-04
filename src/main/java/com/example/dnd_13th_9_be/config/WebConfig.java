69b609fc71691f047aeaebb203a4994e298ac75f
youable <youable.framios@gmail.com>
2025-08-04 23:45:30 +0900

    feat: #13 프론트 localhost에서 호출시 CORS 가능하도록 추가

---
+++ src/main/java/com/example/dnd_13th_9_be/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.dnd_13th_9_be.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOrigins("http://localhost:3000")
+        .allowedMethods("GET", "POST")
+        .allowCredentials(true);
+  }
+}
