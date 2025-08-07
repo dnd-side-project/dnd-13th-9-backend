5b31f3e4c049439564ab7faccfa623a6a6852fa1
youable <youable.framios@gmail.com>
2025-08-07 17:39:35 +0900

    feat: #16 CollectionEntity, FolderEntity 작성

--- src/main/java/com/example/dnd_13th_9_be/collection/persistence/CollectionEntity.java
+++ src/main/java/com/example/dnd_13th_9_be/collection/persistence/CollectionEntity.java
@@ -1,2 +1,47 @@
-package com.example.dnd_13th_9_be.collection.persistence;public class CollectionEntity {
+package com.example.dnd_13th_9_be.collection.persistence;
+
+import com.example.dnd_13th_9_be.folder.persistence.FolderEntity;
+import com.example.dnd_13th_9_be.user.persistence.UserEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "collection")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CollectionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Comment("컬렉션 이름")
+    @Column(nullable = false)
+    private String name;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "collection", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<FolderEntity> folders;
 }
--- src/main/java/com/example/dnd_13th_9_be/folder/persistence/FolderEntity.java
+++ src/main/java/com/example/dnd_13th_9_be/folder/persistence/FolderEntity.java
@@ -1,2 +1,34 @@
-package com.example.dnd_13th_9_be.folder.persistence;public class FolderEntity {
+package com.example.dnd_13th_9_be.folder.persistence;
+
+import com.example.dnd_13th_9_be.collection.persistence.CollectionEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "folder")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FolderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "collection_id", nullable = false)
+    private CollectionEntity collection;
+
+    @Comment("폴더 명")
+    @Column(nullable = false)
+    private String name;
 }
