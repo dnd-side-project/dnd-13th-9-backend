a951ecbc3062e6102c25b562c80ab92a68fed2b9
youable <youable.framios@gmail.com>
2025-08-07 19:30:33 +0900

    feat: 체크리스트 관련 엔티티 설계

---
+++ src/main/java/com/example/dnd_13th_9_be/checklist/persistence/ChecklistCategoryEntity.java
@@ -0,0 +1,34 @@
+package com.example.dnd_13th_9_be.checklist.persistence;
+
+import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "checklist_category")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChecklistCategoryEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Comment("메인 공간, 창문, 건물")
+  @Column(nullable = false, length = 10)
+  private String name;
+
+  @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<ChecklistItemEntity> items;
+}
---
+++ src/main/java/com/example/dnd_13th_9_be/checklist/persistence/ChecklistItemEntity.java
@@ -0,0 +1,54 @@
+package com.example.dnd_13th_9_be.checklist.persistence;
+
+import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "checklist_item")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChecklistItemEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "category_id", nullable = false)
+  private ChecklistCategoryEntity category;
+
+  @Comment("질문")
+  @Column(nullable = false, length = 100)
+  private String question;
+
+  @Comment("질문 설명")
+  @Column(length = 100)
+  private String description;
+
+  @Comment("체크리스트 옵션 값")
+  @OneToMany(mappedBy = "item", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<ChecklistOptionEntity> options;
+
+  @Comment("사용자 지정 필수 확인")
+  @OneToMany(mappedBy = "item", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<UserChecklistFavoriteEntity> favorites;
+
+  @Comment("매물 정보에서 선택한 아이템")
+  @OneToMany(mappedBy = "item", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<PropertyChecklistAnswerEntity> answers;
+}
