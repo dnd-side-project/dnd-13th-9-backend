79652018f820ba6dcd11077b9afc065a67da91df
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-11 22:59:54 +0900

    feat : AppProperties 생성

---
+++ src/main/java/com/example/dnd_13th_9_be/config/AppProperties.java
@@ -0,0 +1,38 @@
+package com.example.dnd_13th_9_be.config;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+    private Security security;
+
+    @Setter
+    @Getter
+    public static class Security{
+        private Jwt jwt;
+        private Oauth2 oauth2;
+
+        @Setter
+        @Getter
+        public static class Jwt{
+            private String secret;
+            private long accessTokenExpirationMs;
+            private long refreshTokenExpirationMs;
+        }
+
+        @Setter
+        @Getter
+        public static class Oauth2{
+            private String frontendRedirectBaseUrl;
+        }
+    }
+
+}
