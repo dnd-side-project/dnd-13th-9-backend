e8e880627afea28df675dc0593e94c51104a93b2
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-12 23:46:33 +0900

    feat : InvalidTokenException 예외 추가

---
+++ src/main/java/com/example/dnd_13th_9_be/global/error/InvalidTokenException.java
@@ -0,0 +1,20 @@
+package com.example.dnd_13th_9_be.global.error;
+
+public class InvalidTokenException extends BusinessException{
+
+    public InvalidTokenException(){
+        super(ErrorCode.TOKEN_NOT_FOUND);
+    }
+
+    public InvalidTokenException(String message) {
+        super(ErrorCode.TOKEN_NOT_FOUND, message);
+    }
+
+    public InvalidTokenException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public InvalidTokenException(ErrorCode errorCode, String message) {
+        super(errorCode, message);
+    }
+}
