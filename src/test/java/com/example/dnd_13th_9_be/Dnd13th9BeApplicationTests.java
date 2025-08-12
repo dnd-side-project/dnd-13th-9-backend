939f72ca0c30c2c72f7f739919a16b824cae23dc
youable <youable.framios@gmail.com>
2025-08-12 23:27:27 +0900

    fix: CI DB 미설정 수정

--- src/test/java/com/example/dnd_13th_9_be/Dnd13th9BeApplicationTests.java
+++ src/test/java/com/example/dnd_13th_9_be/Dnd13th9BeApplicationTests.java
@@ -3,8 +3,10 @@ package com.example.dnd_13th_9_be;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Dnd13th9BeApplicationTests {
 
   @Test
