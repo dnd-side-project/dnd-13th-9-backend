377b77f79095fdf2d2e5ea81dadff44421a5f9c3
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-14 14:25:58 +0900

    feat : JWTFilter 구현

---
+++ src/main/java/com/example/dnd_13th_9_be/config/security/JWTFilter.java
@@ -0,0 +1,66 @@
+package com.example.dnd_13th_9_be.config.security;
+
+import java.io.IOException;
+import java.util.Optional;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import com.example.dnd_13th_9_be.common.utils.CookieUtil;
+import com.example.dnd_13th_9_be.global.error.JwtAuthenticationException;
+import com.example.dnd_13th_9_be.user.application.dto.UserPrincipleDto;
+import com.example.dnd_13th_9_be.user.application.token.JwtTokenValidatorService;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JWTFilter extends OncePerRequestFilter {
+
+  private final AuthenticationEntryPoint entryPoint;
+  private final JwtTokenValidatorService jwtTokenValidatorService;
+  private final CookieUtil cookieUtil;
+
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    if (request.getRequestURI().startsWith("/login/oauth2/")
+        || request.getRequestURI().startsWith("/oauth2/authorization")
+        || request.getRequestURI().equals("/api/auth/refresh")
+        || request.getRequestURI().equals("/api/test/auth/token")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+    
+
+    try {
+
+      Optional<Cookie> OptionalCookie = cookieUtil.find(request.getCookies(), "Access");
+      Cookie cookie =
+          OptionalCookie.orElseThrow(() -> new IllegalStateException("Access Cookie not found"));
+      String accessToken = cookie.getValue();
+      UserPrincipleDto userPrincipleDto = jwtTokenValidatorService.validateToken(accessToken);
+      Authentication authentication =
+          new UsernamePasswordAuthenticationToken(
+              userPrincipleDto, null, userPrincipleDto.getAuthorities());
+
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+      filterChain.doFilter(request, response);
+    } catch (Exception e) {
+      log.info("JWT 필터에서 Access 토큰 검증 도중 예외 발생 -> {}", e.getMessage());
+
+      entryPoint.commence(request, response, new JwtAuthenticationException(e.getMessage(), e) {});
+    }
+  }
+}
