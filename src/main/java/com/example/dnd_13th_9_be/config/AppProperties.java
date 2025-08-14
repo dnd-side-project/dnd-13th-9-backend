a6876f2f59dc50319bddb191f15a834d76e3fbce
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-14 14:21:32 +0900

    feat : AppProperties 구현

--- src/main/java/com/example/dnd_13th_9_be/config/AppProperties.java
+++ src/main/java/com/example/dnd_13th_9_be/config/AppProperties.java
@@ -1,38 +1,36 @@
 package com.example.dnd_13th_9_be.config;
 
-
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Getter;
+import lombok.Setter;
 
 @Setter
 @Getter
 @Configuration
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
-    private Security security;
+  private Security security;
+
+  @Setter
+  @Getter
+  public static class Security {
+    private Jwt jwt;
+    private Oauth2 oauth2;
 
     @Setter
     @Getter
-    public static class Security{
-        private Jwt jwt;
-        private Oauth2 oauth2;
-
-        @Setter
-        @Getter
-        public static class Jwt{
-            private String secret;
-            private long accessTokenExpirationMs;
-            private long refreshTokenExpirationMs;
-        }
-
-        @Setter
-        @Getter
-        public static class Oauth2{
-            private String frontendRedirectBaseUrl;
-        }
+    public static class Jwt {
+      private String secret;
+      private long accessTokenExpirationMs;
+      private long refreshTokenExpirationMs;
     }
 
+    @Setter
+    @Getter
+    public static class Oauth2 {
+      private String frontendRedirectBaseUrl;
+    }
+  }
 }
