1a95e4d037fcd0e43eb34a20539c98dc7c79f2ab
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-14 14:33:49 +0900

    feat : UserRepositoryImpl 구현

--- src/main/java/com/example/dnd_13th_9_be/user/persistence/UserRepositoryImpl.java
+++ src/main/java/com/example/dnd_13th_9_be/user/persistence/UserRepositoryImpl.java
@@ -1,3 +1,35 @@
 package com.example.dnd_13th_9_be.user.persistence;
 
-public class UserRepositoryImpl {}
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.AllArgsConstructor;
+
+import com.example.dnd_13th_9_be.user.application.model.UserModel;
+import com.example.dnd_13th_9_be.user.application.model.converter.UserConverter;
+import com.example.dnd_13th_9_be.user.application.repository.UserRepository;
+
+@Repository
+@AllArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+  private final JpaUserRepository jpaUserRepository;
+  private final UserConverter userConverter;
+
+  @Override
+  public Optional<UserModel> findById(Long userId) {
+    return jpaUserRepository.findById(userId).map(userConverter::from);
+  }
+
+  @Override
+  public Optional<UserModel> findByProviderId(String providerId) {
+    return jpaUserRepository.findByProviderId(providerId).map(userConverter::from);
+  }
+
+  @Override
+  public UserModel save(UserModel userModel) {
+    User user = userConverter.toEntity(userModel);
+    User savedUser = jpaUserRepository.save(user);
+    return userConverter.from(savedUser);
+  }
+}
