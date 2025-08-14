0aa81758468ac642e467674ce201caf9e358a444
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-14 14:22:09 +0900

    feat : BaseEntity 구현

--- src/main/java/com/example/dnd_13th_9_be/common/persistence/BaseEntity.java
+++ src/main/java/com/example/dnd_13th_9_be/common/persistence/BaseEntity.java
@@ -1,19 +1,16 @@
 package com.example.dnd_13th_9_be.common.persistence;
 
+import java.sql.Timestamp;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.*;
-import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.sql.Timestamp;
+import lombok.*;
 
-
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -22,13 +19,11 @@ import java.sql.Timestamp;
 @EntityListeners({AuditingEntityListener.class})
 public class BaseEntity {
 
+  @Column(nullable = false, updatable = false)
+  @CreationTimestamp
+  private Timestamp createdAt;
 
-    @Column(nullable = false, updatable = false)
-    @CreationTimestamp
-    private Timestamp createdAt;
-
-    @Column(nullable = false)
-    @UpdateTimestamp
-    private Timestamp updatedAt;
-
+  @Column(nullable = false)
+  @UpdateTimestamp
+  private Timestamp updatedAt;
 }
