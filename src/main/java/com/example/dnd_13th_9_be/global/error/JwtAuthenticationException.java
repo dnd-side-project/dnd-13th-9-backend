49acec54b90a6b9da3411dcafb20b1de6310f3d1
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-14 14:25:37 +0900

    feat : JwtAuthenticationException 추가

--- src/main/java/com/example/dnd_13th_9_be/global/error/JwtAuthenticationException.java
+++ src/main/java/com/example/dnd_13th_9_be/global/error/JwtAuthenticationException.java
@@ -3,11 +3,11 @@ package com.example.dnd_13th_9_be.global.error;
 import org.springframework.security.core.AuthenticationException;
 
 public class JwtAuthenticationException extends AuthenticationException {
-    public JwtAuthenticationException(String message) {
-        super(message);
-    }
+  public JwtAuthenticationException(String message) {
+    super(message);
+  }
 
-    public JwtAuthenticationException(String message, Throwable cause) {
-        super(message, cause);
-    }
+  public JwtAuthenticationException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
