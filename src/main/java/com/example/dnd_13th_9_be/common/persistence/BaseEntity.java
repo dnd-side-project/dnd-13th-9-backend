46bba29258d5565443e92ed0249c44847b1d5f53
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-14 14:18:11 +0900

    feat : BaseEntity 추가

--- src/main/java/com/example/dnd_13th_9_be/common/persistence/BaseEntity.java
+++ src/main/java/com/example/dnd_13th_9_be/common/persistence/BaseEntity.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -18,15 +20,15 @@ import java.sql.Timestamp;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @MappedSuperclass
 @EntityListeners({AuditingEntityListener.class})
-@SuperBuilder(toBuilder = true)
 public class BaseEntity {
 
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private Timestamp createdDate;
 
-    @LastModifiedDate
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
     @Column(nullable = false)
-    private Timestamp updatedDate;
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 
 }
