1618485e0a0441fb3ce1a2e443506b1367fd3817
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-14 14:33:30 +0900

    feat : UserRepository 추가

---
+++ src/main/java/com/example/dnd_13th_9_be/user/application/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.dnd_13th_9_be.user.application.repository;
+
+import java.util.Optional;
+
+import com.example.dnd_13th_9_be.user.application.model.UserModel;
+
+public interface UserRepository {
+
+  Optional<UserModel> findById(Long userId);
+
+  Optional<UserModel> findByProviderId(String providerId);
+
+  UserModel save(UserModel userModel);
+}
