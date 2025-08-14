7288c27b28062f9e370c8e1f70b9366d49e9e6ee
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-14 14:23:41 +0900

    chore : spotless 적용

--- src/main/java/com/example/dnd_13th_9_be/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/example/dnd_13th_9_be/config/security/CustomAuthenticationEntryPoint.java
@@ -1,15 +1,17 @@
 package com.example.dnd_13th_9_be.config.security;
 
-import com.example.dnd_13th_9_be.global.response.ApiResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+
+import com.example.dnd_13th_9_be.global.response.ApiResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static com.example.dnd_13th_9_be.global.error.ErrorCode.TOKEN_UNAUTHORIZED;
 
@@ -17,25 +19,28 @@ import static com.example.dnd_13th_9_be.global.error.ErrorCode.TOKEN_UNAUTHORIZE
 @RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private final ObjectMapper objectMapper;
+  private final ObjectMapper objectMapper;
 
-    @Override
-    public void commence(HttpServletRequest request,
-                         HttpServletResponse response,
-                         AuthenticationException authException) throws IOException {
+  @Override
+  public void commence(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      AuthenticationException authException)
+      throws IOException {
 
-        String xhrHeader = request.getHeader("X-Requested-With");
-        String accept = request.getHeader("Accept");
+    String xhrHeader = request.getHeader("X-Requested-With");
+    String accept = request.getHeader("Accept");
 
-        boolean isAjax = "XMLHttpRequest".equalsIgnoreCase(xhrHeader)
-                || (accept != null && accept.contains("application/json"));
+    boolean isAjax =
+        "XMLHttpRequest".equalsIgnoreCase(xhrHeader)
+            || (accept != null && accept.contains("application/json"));
 
-        if (isAjax) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.setContentType("application/json");
+    if (isAjax) {
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      response.setContentType("application/json");
 
-            String apiResponse = objectMapper.writeValueAsString(ApiResponse.error(TOKEN_UNAUTHORIZED));
-            response.getWriter().write(apiResponse);
-        }
+      String apiResponse = objectMapper.writeValueAsString(ApiResponse.error(TOKEN_UNAUTHORIZED));
+      response.getWriter().write(apiResponse);
     }
+  }
 }
