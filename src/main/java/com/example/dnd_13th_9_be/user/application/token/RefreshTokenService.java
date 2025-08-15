70dffb59f2aafe03f6da06c605eca9eaebc178a3
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-16 02:29:21 +0900

    refactor : exception 수정
    도메인별 exception을 던지도록 에러 코드 생성, 수정하였습니다.

--- src/main/java/com/example/dnd_13th_9_be/user/application/token/RefreshTokenService.java
+++ src/main/java/com/example/dnd_13th_9_be/user/application/token/RefreshTokenService.java
@@ -2,6 +2,7 @@ package com.example.dnd_13th_9_be.user.application.token;
 
 import java.time.Instant;
 
+import com.example.dnd_13th_9_be.global.error.UserNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,7 @@ import com.example.dnd_13th_9_be.user.persistence.JpaUserRepository;
 import com.example.dnd_13th_9_be.user.persistence.RefreshToken;
 import com.example.dnd_13th_9_be.user.persistence.User;
 
+import static com.example.dnd_13th_9_be.global.error.ErrorCode.*;
 import static com.example.dnd_13th_9_be.user.persistence.QUser.user;
 
 @Service
@@ -26,7 +28,7 @@ public class RefreshTokenService {
   private final JpaRefreshTokenRepository jpaRefreshTokenRepository;
   private final JpaUserRepository jpaUserRepository;
 
-  // TODO: 모델
+  // TODO: 영속화 리팩토링
   @Transactional
   public void createRefreshToken(String token, Instant expiration, UserModel userModel) {
     Long userId = userModel.getId();
@@ -36,7 +38,7 @@ public class RefreshTokenService {
       jpaRefreshTokenRepository.flush();
 
       User userEntity = jpaUserRepository.findById(userId)
-              .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다"));
+              .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
 
       RefreshToken refreshTokenEntity = RefreshToken.builder()
               .token(token)
@@ -46,27 +48,20 @@ public class RefreshTokenService {
 
       jpaRefreshTokenRepository.save(refreshTokenEntity);
     } catch (Exception e) {
-      throw new RuntimeException("리프레시 토큰 생성에 실패했습니다", e);
+      throw new InvalidTokenException(REFRESH_TOKEN_NOT_CREATED);
     }
   }
 
   @Transactional
   public RefreshTokenModel getRefreshToken(String token) {
-    RefreshTokenModel refreshToken = refreshTokenRepository.findByToken(token);
-    if (refreshToken.isInValid()) {
-      throw new InvalidTokenException("유효하지 않은 리프레시 토큰입니다.");
-    }
-    return refreshToken;
+    return refreshTokenRepository.findByToken(token)
+            .filter(RefreshTokenModel::isValid)
+            .orElseThrow(() -> new InvalidTokenException(INVALID_REFRESH_TOKEN));
   }
 
   @Transactional
   public UserModel findUserByRefreshToken(String token) {
-    RefreshTokenModel refreshTokenModel = refreshTokenRepository.findByToken(token);
-
-    if (refreshTokenModel.isInValid()) {
-      throw new InvalidTokenException("만료된 리프레시 토큰입니다.");
-    }
-
+    RefreshTokenModel refreshTokenModel = getRefreshToken(token);
     return refreshTokenModel.getUser();
   }
 
