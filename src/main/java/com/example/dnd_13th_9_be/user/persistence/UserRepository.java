708b36624d45e49ca146e693cc07a3ddaf183ba2
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-16 02:46:29 +0900

    remove : userRepository 제거

--- src/main/java/com/example/dnd_13th_9_be/user/persistence/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dnd_13th_9_be.user.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
