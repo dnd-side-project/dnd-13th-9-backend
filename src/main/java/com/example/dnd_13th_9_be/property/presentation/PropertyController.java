499f1b9406bc648e0c61ab2146fc16bc431071b2
youable <youable.framios@gmail.com>
2025-08-25 00:11:39 +0900

    fix: spotlessApply

--- src/main/java/com/example/dnd_13th_9_be/property/presentation/PropertyController.java
+++ src/main/java/com/example/dnd_13th_9_be/property/presentation/PropertyController.java
@@ -3,7 +3,6 @@ package com.example.dnd_13th_9_be.property.presentation;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
