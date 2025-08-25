fb894e9bd7d46fb43de49f2433dbf3ef3b4a2c6f
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-25 23:35:48 +0900

    feat : PlaceMemoConverter 생성

---
+++ src/main/java/com/example/dnd_13th_9_be/placeMemo/persistence/converter/PlaceMemoConverter.java
@@ -0,0 +1,66 @@
+package com.example.dnd_13th_9_be.placeMemo.persistence.converter;
+
+import com.example.dnd_13th_9_be.common.support.converter.AbstractEntityConverter;
+import com.example.dnd_13th_9_be.folder.persistence.entity.Folder;
+import com.example.dnd_13th_9_be.placeMemo.application.model.PlaceMemoModel;
+import com.example.dnd_13th_9_be.placeMemo.persistence.PlaceMemo;
+import com.example.dnd_13th_9_be.placeMemo.persistence.PlaceMemoImage;
+import com.example.dnd_13th_9_be.user.persistence.User;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+public class PlaceMemoConverter implements AbstractEntityConverter<PlaceMemo, PlaceMemoModel> {
+
+
+
+    @Override
+    public PlaceMemoModel from(PlaceMemo placeMemo) {
+        return PlaceMemoModel.builder()
+                .id(placeMemo.getId())
+                .title(placeMemo.getTitle())
+                .placeTag(placeMemo.getPlaceTag())
+                .description(placeMemo.getDescription())
+                .folderId(placeMemo.getFolder().getId())
+                .address(placeMemo.getAddress())
+                .latitude(placeMemo.getLatitude())
+                .longitude(placeMemo.getLongitude())
+                .imageUrls(placeMemo.getImages() == null ? List.of()
+                        : placeMemo.getImages().stream()
+                        .map(PlaceMemoImage::getImageUrl)
+                        .toList())
+                .build();
+    }
+
+    @Override
+    public PlaceMemo toEntity(PlaceMemoModel m) {
+        return PlaceMemo.builder()
+                .title(m.getTitle())
+                .placeTag(m.getPlaceTag())
+                .description(m.getDescription())
+                .address(m.getAddress())
+                .latitude(m.getLatitude())
+                .longitude(m.getLongitude())
+                .images(m.getImageUrls().stream()
+                        .map(url -> PlaceMemoImage.builder().imageUrl(url).build())
+                        .toList())
+                .build();
+    }
+
+
+    public PlaceMemo toEntity(PlaceMemoModel placeMemoModel, User user, Folder folder) {
+        return PlaceMemo.builder()
+                .user(user)
+                .folder(folder)
+                .title(placeMemoModel.getTitle())
+                .placeTag(placeMemoModel.getPlaceTag())
+                .description(placeMemoModel.getDescription())
+                .address(placeMemoModel.getAddress())
+                .latitude(placeMemoModel.getLatitude())
+                .longitude(placeMemoModel.getLongitude())
+                .images(placeMemoModel.getImageUrls().stream()
+                        .map(url -> PlaceMemoImage.builder().imageUrl(url).build())
+                        .toList())
+                .build();
+    }
+}
