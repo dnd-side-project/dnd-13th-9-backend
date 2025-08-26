f32bd77545bbf4063f57c9324ce00464dd3a0be5
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-26 19:02:28 +0900

    feat : placeMemoController 생성, 단건조회, 삭제, 수정, 리스트 조회 api 구현

--- src/main/java/com/example/dnd_13th_9_be/placeMemo/presentation/PlaceMemoController.java
+++ src/main/java/com/example/dnd_13th_9_be/placeMemo/presentation/PlaceMemoController.java
@@ -1,4 +1,68 @@
 package com.example.dnd_13th_9_be.placeMemo.presentation;
 
-public class PlaceMemoController {
+import com.example.dnd_13th_9_be.global.response.ApiResponse;
+import com.example.dnd_13th_9_be.placeMemo.application.dto.*;
+import com.example.dnd_13th_9_be.placeMemo.application.service.PlaceMemoService;
+import com.example.dnd_13th_9_be.placeMemo.presentation.docs.PlaceMemoDocs;
+import com.example.dnd_13th_9_be.user.application.dto.UserPrincipalDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/placeMemo")
+@RequiredArgsConstructor
+public class PlaceMemoController implements PlaceMemoDocs {
+
+    private final PlaceMemoService placeMemoService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<CreatePlaceMemoResponse>> create(
+            @AuthenticationPrincipal UserPrincipalDto userPrincipalDto,
+            @Valid @ModelAttribute CreatePlaceMemoRequest request
+            ){
+        CreatePlaceMemoResponse response = placeMemoService.create(userPrincipalDto.getUserId(), request);
+        return ApiResponse.successEntity(response);
+    }
+
+    @GetMapping("/{placeMemoId}")
+    public ResponseEntity<ApiResponse<QueryPlaceMemoResponse>> findOne(
+            @AuthenticationPrincipal UserPrincipalDto userPrincipalDto,
+            @PathVariable("placeMemoId") Long placeMemoId){
+        QueryPlaceMemoResponse response = placeMemoService.findOne(placeMemoId, userPrincipalDto.getUserId());
+        return ApiResponse.successEntity(response);
+
+    }
+
+    @DeleteMapping("/{placeMemoId}")
+    public ResponseEntity<ApiResponse<String>> delete(
+            @AuthenticationPrincipal UserPrincipalDto userPrincipalDto,
+            @PathVariable("placeMemoId") Long placeMemoId){
+        placeMemoService.delete(placeMemoId, userPrincipalDto.getUserId());
+        return ApiResponse.successEntity("삭제되었습니다");
+    }
+
+    @PutMapping("/{placeMemoId}")
+    public ResponseEntity<ApiResponse<String>> update(
+            @AuthenticationPrincipal UserPrincipalDto userPrincipalDto,
+            @PathVariable("placeMemoId") Long placeMemoId,
+            @Valid @ModelAttribute UpdatePlaceMemoRequest request) {
+        placeMemoService.update(placeMemoId, userPrincipalDto.getUserId(), request);
+        return ApiResponse.successEntity("수정완료");
+    }
+
+    @GetMapping("/folders/{folderId}")
+    public ResponseEntity<ApiResponse<QueryPlaceMemoListResponse>> findAll(
+            @AuthenticationPrincipal UserPrincipalDto userPrincipalDto,
+            @PathVariable("folderId") Long folderId
+    ){
+        QueryPlaceMemoListResponse response = placeMemoService.findAll(folderId, userPrincipalDto.getUserId());
+        return ApiResponse.successEntity(response);
+    }
+
+
+
+
 }
