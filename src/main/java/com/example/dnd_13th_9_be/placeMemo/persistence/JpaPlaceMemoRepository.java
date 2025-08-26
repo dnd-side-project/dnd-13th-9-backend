3f8aa804bb485fa8bcf28d052012234aa49ae2b6
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-26 19:01:17 +0900

    feat : JpaPlaceMemoRepository 메서드 추가

--- src/main/java/com/example/dnd_13th_9_be/placeMemo/persistence/JpaPlaceMemoRepository.java
+++ src/main/java/com/example/dnd_13th_9_be/placeMemo/persistence/JpaPlaceMemoRepository.java
@@ -1,4 +1,16 @@
 package com.example.dnd_13th_9_be.placeMemo.persistence;
 
-public interface JpaPlaceMemoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JpaPlaceMemoRepository extends JpaRepository<PlaceMemo, Long> {
+
+    Optional<PlaceMemo> findByIdAndUserId(Long placeMemoId, Long userId);
+
+    List<PlaceMemo> findByFolderIdAndUserIdOrderByIdDesc(Long folderId, Long userId);
+
+
+
 }
