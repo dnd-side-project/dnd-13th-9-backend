935c817ea20cbadbfcc3c7889ae466a1af916232
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-26 19:04:02 +0900

    feat : application placeMemoRepository 인터페이스 생성

--- src/main/java/com/example/dnd_13th_9_be/placeMemo/application/repository/PlaceMemoRepository.java
+++ src/main/java/com/example/dnd_13th_9_be/placeMemo/application/repository/PlaceMemoRepository.java
@@ -1,4 +1,23 @@
 package com.example.dnd_13th_9_be.placeMemo.application.repository;
 
-public class PlaceMemoRepository {
+import com.example.dnd_13th_9_be.folder.persistence.entity.Folder;
+import com.example.dnd_13th_9_be.placeMemo.application.model.PlaceMemoModel;
+import com.example.dnd_13th_9_be.placeMemo.persistence.PlaceMemo;
+import com.example.dnd_13th_9_be.user.persistence.User;
+import org.springframework.security.core.parameters.P;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PlaceMemoRepository {
+
+    PlaceMemoModel save(Long userId, PlaceMemoModel placeMemoModel);
+
+    Optional<PlaceMemoModel> findByIdAndUserId(Long placeMemoId, Long userId);
+
+    void deleteById(Long placeMemoId, Long UserId);
+
+    List<PlaceMemoModel> findByFolderIdAndUserId(Long folderId, Long userId);
+
+    void update(Long userId, Long placeMemoId, PlaceMemoModel model);
 }
