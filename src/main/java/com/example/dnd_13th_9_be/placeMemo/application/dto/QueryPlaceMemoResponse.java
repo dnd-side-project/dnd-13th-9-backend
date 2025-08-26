1d0f634abf50bb015632fd04ef1db8f82a9ae39b
Daae-Kim <wlsemrdl1212@naver.com>
2025-08-26 19:06:49 +0900

    refactor : QueryPlaceMemoResponse 구현

---
+++ src/main/java/com/example/dnd_13th_9_be/placeMemo/application/dto/QueryPlaceMemoResponse.java
@@ -0,0 +1,56 @@
+package com.example.dnd_13th_9_be.placeMemo.application.dto;
+
+import com.example.dnd_13th_9_be.placeMemo.application.model.PlaceMemoModel;
+import com.example.dnd_13th_9_be.placeMemo.persistence.PlaceTag;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+
+import java.util.List;
+
+
+@Builder(toBuilder = true)
+public record QueryPlaceMemoResponse (
+
+    @Schema(description = " 주변 메모 ID", example = "1")
+    Long id,
+
+    @Schema(description = "장소 이름", example = "강남 카페")
+    String title,
+
+    @Schema(description = "장소 태그", example = "CONVENIENCE")
+    PlaceTag placeTag,
+
+    @Schema(description = "메모 내용", example = "조용하고 분위기 좋은 카페")
+    String description,
+
+    @Schema(description = "주소", example = "서울 강남구 테헤란로 123")
+    String address,
+
+    @Schema(description = "위도", example = "37.12345")
+    String latitude,
+
+    @Schema(description = "경도", example = "127.12345")
+    String longitude,
+
+    @Schema(description = "폴더 ID", example = "1")
+    Long folderId,
+
+    @Schema(description = "이미지 URL 리스트")
+    List<String> imageUrls
+){
+
+    public static QueryPlaceMemoResponse from(PlaceMemoModel placeMemoModel){
+        return QueryPlaceMemoResponse.builder()
+                .id(placeMemoModel.getId())
+                .title(placeMemoModel.getTitle())
+                .placeTag(placeMemoModel.getPlaceTag())
+                .description(placeMemoModel.getDescription())
+                .address(placeMemoModel.getAddress())
+                .latitude(placeMemoModel.getLatitude())
+                .longitude(placeMemoModel.getLongitude())
+                .folderId(placeMemoModel.getFolderId())
+                .imageUrls(placeMemoModel.getImageUrls() == null ? List.of() : List.copyOf(placeMemoModel.getImageUrls()))
+                .build();
+    }
+}
