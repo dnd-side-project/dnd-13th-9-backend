e4667fdb452be6409da6ed72c0bfbcafcc2a36f1
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2025-08-27 19:30:28 +0900

    chore : spotlessApply

--- src/main/java/com/example/dnd_13th_9_be/folder/application/dto/RecordSummaryResult.java
+++ src/main/java/com/example/dnd_13th_9_be/folder/application/dto/RecordSummaryResult.java
@@ -28,7 +28,10 @@ public record RecordSummaryResult(
 
     return RecordSummaryResult.builder()
         .id(recordSummary.id())
-        .imageUrl(recordSummary.images() !=null? recordSummary.images().stream().map(RecordImageSummary::url).toList() : List.of())
+        .imageUrl(
+            recordSummary.images() != null
+                ? recordSummary.images().stream().map(RecordImageSummary::url).toList()
+                : List.of())
         .recordType(recordSummary.recordType() != null ? recordSummary.recordType().name() : null)
         .feeling(recordSummary.feeling() != null ? recordSummary.feeling().name() : null)
         .title(recordSummary.title())
